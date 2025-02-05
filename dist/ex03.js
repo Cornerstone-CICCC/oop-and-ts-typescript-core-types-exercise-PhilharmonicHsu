0e52a7530b1151f5cdc68aa03ba8217a6dd16724
Phil <xuyuwei19940909@gmail.com>
2025-02-05 14:05:34 -0800

    done

--- dist/ex03.js
+++ dist/ex03.js
@@ -1,14 +1,12 @@
+"use strict";
 // Exercise: Write a function `logValues` that takes an array of any type of values (numbers, strings, booleans, etc.).
 // The function should iterate through the array and log each value along with its type.
-
-
-function logValues(values: [number, string, boolean, {name: string}]) {
-    console.log(values[0], "(number)")
-    console.log(values[1], "(string)")
-    console.log(values[2], "(boolean)")
-    console.log(values[3], "(object)")
+function logValues(values) {
+    console.log(values[0], "(number)");
+    console.log(values[1], "(string)");
+    console.log(values[2], "(boolean)");
+    console.log(values[3], "(object)");
 }
-
 logValues([42, "Hello", true, { name: "Alice" }]);
 // Expected output:
 // 42 (number)
